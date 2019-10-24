ba1069de9534d515518cae950b43f6408238d25d
Ludovic16513 <56023526+Ludovic16513@users.noreply.github.com>
2019-10-24 14:16:04 +0200

    Ludo

---
+++ historique.js
@@ -0,0 +1,23 @@
+document.getElementById("click1").addEventListener("mouseover", function() {
+    document.getElementById("i1").style.opacity="1";
+});
+
+document.getElementById("click1").addEventListener("mouseout", function() {
+    document.getElementById("i1").style.opacity="0";
+});
+
+document.getElementById("click2").addEventListener("mouseover", function() {
+    document.getElementById("i2").style.opacity="1";
+});
+
+document.getElementById("click2").addEventListener("mouseout", function() {
+    document.getElementById("i2").style.opacity="0";
+});
+
+document.getElementById("click3").addEventListener("mouseover", function() {
+    document.getElementById("i3").style.opacity="1";
+});
+
+document.getElementById("click3").addEventListener("mouseout", function() {
+    document.getElementById("i3").style.opacity="0";
+});
